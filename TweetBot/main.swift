4d14464d277c36259d45737cb7beebef35b301ce
Anthony Picciano <anthony.picciano@gmail.com>
2014-10-27 12:12:10 -0600

    Initial Commit

---
+++ TweetBot/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  TweetBot
+//
+//  Created by Anthony Picciano on 10/27/14.
+//  Copyright (c) 2014 Crispin Porter + Bogusky. All rights reserved.
+//
+
+import Foundation
+
+println("Hello, World!")
+
